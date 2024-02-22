685cf595cb902ed9a113e10f49a33e045c1e1915
RACHIDKM <rkarym7@gmail.com>
2024-02-22 17:02:18 +0100

    up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 require('dotenv').config()
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile("./Views/home.html", { root: __dirname })
@@ -11,8 +11,24 @@ app.get('/', (req, res) => {
 })
 
 
-mongoose.connect(`mongodb+srv://rkarym7:${process.env.password}@backend.hmf7ygu.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://rkarym7:${process.env.password}@backend.hmf7ygu.mongodb.net/all-articles?retryWrites=true&w=majority`)
 .then(() => { app.listen(port, () => {
  console.log(`http://localhost:${port}`)
     })
 }).catch ((err) => { console.log(err); })
+
+
+
+const Article = require("./models/articleSchema");
+app.post("/all-articles", (req, res) => {
+  const article = new Article(req.body);
+
+  article
+    .save()
+    .then( result => {
+      res.redirect("/");
+    })
+    .catch( err => {
+      console.log(err);
+    });
+});
---
+++ models/articleSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+    const Schema = mongoose.Schema;
+       // define the Schema (the structure of the article)
+       const articleSchema = new Schema({
+        name: String,
+        age: String,
+      });
+      
+      // Create a model based on that schema
+      const Article = mongoose.model("Article", articleSchema);
+      
+      // export the model
+      module.exports = Article;
